85577c658e6d1672f1d8cad6c77a00a86f2cedfa
kapoor <kapoorvishal@gmail.com>
2012-09-05 18:39:32 -0700

    Updating dir structure

--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  belongs_to :topic
-  attr_accessible :posted_on, :poster_id, :text
-  validates :text,  :presence => true, :length => { :minimum => 10 }
-end
--- app/models/topic.rb
+++
@@ -1,5 +0,0 @@
-class Topic < ActiveRecord::Base
-  attr_accessible :posted_on, :poster_id, :title
-  validates :title,  :presence => true, :length => { :minimum => 5 } 
-  has_many :posts, :dependent => :destroy
-end
